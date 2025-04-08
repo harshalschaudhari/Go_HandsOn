fa0a83926138767252f294bbd2e87d0752af4d7d
Harshal Chaudhari <harshalschaudhari@gmail.com>
2025-04-08 21:03:31 +0530

    Go Hello World

---
+++ code/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+// Step: 1
+// PS D:\hc-git\Go_HandsOn\code> go mod init demo
+// go: creating new go.mod: module demo
+
+// Step: 2
+// Create a go program
+
+// Step: 3
+// How to run go program
+// PS D:\hc-git\Go_HandsOn\code> go run .\main.go
+// Hello, World!
+
+//Another way
+// PS D:\hc-git\Go_HandsOn\code> go run .
+// Hello, World!
